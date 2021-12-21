45b1d54b272557bea6fdeb851a6ef9034bdec787
IMDroidude <zareahmer89@gmail.com>
2021-12-21 17:34:04 +0500

    - Library commit... 1.0.6

--- app/src/main/java/planetboyond/pushlibrarytomaven/MainActivity.kt
+++ app/src/main/java/planetboyond/pushlibrarytomaven/MainActivity.kt
@@ -2,12 +2,14 @@ package planetboyond.pushlibrarytomaven
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import planetbeyond.pblib.Toasting
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        ///val toasting:Toasting()
+        val toasting = Toasting()
+
     }
 }
