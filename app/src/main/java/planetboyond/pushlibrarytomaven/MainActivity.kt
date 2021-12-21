f4088bba23de256ea16959221b9ecd88152d994a
IMDroidude <zareahmer89@gmail.com>
2021-12-21 15:13:21 +0500

    - initial commit

---
+++ app/src/main/java/planetboyond/pushlibrarytomaven/MainActivity.kt
@@ -0,0 +1,11 @@
+package planetboyond.pushlibrarytomaven
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
